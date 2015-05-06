b4ca7310f1253c060624a6d142b0e6e0601b214f
Denis Surzhan <denis.surzhan@gmail.com>
2015-05-06 19:49:45 +0300

    IDPStudy Project -> initial commit

---
+++ DSHomeWork/DSHomeWork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DSHomeWork
+//
+//  Created by Denis Surzhan on 06.05.15.
+//  Copyright (c) 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
