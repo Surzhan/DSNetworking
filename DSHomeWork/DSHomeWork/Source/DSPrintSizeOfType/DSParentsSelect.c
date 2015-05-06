ae8e32322acf9140967e350c79632c736cfcad14
Denis Surzhan <denis.surzhan@gmail.com>
2015-05-06 20:09:13 +0300

    DSHomework1 -> add new group

---
+++ DSHomeWork/DSHomeWork/Source/DSPrintSizeOfType/DSParentsSelect.c
@@ -0,0 +1,9 @@
+//
+//  DSParentsSelect.c
+//  DSHomeWork
+//
+//  Created by Denis Surzhan on 06.05.15.
+//  Copyright (c) 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#include "DSParentsSelect.h"
---
+++ DSHomeWork/DSHomeWork/Source/DSPrintSizeOfType/DSParentsSelect.h
@@ -0,0 +1,14 @@
+//
+//  DSParentsSelect.h
+//  DSHomeWork
+//
+//  Created by Denis Surzhan on 06.05.15.
+//  Copyright (c) 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#ifndef __DSHomeWork__DSParentsSelect__
+#define __DSHomeWork__DSParentsSelect__
+
+#include <stdio.h>
+
+#endif /* defined(__DSHomeWork__DSParentsSelect__) */
