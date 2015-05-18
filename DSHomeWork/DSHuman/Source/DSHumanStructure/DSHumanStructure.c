1caf3376b94df8960fc519463be06058a19c4cc9
Denis Surzhan <denis.surzhan@gmail.com>
2015-05-18 22:45:00 +0300

    DSHumanStructure.h -> add struct DSHuman

---
+++ DSHomeWork/DSHuman/Source/DSHumanStructure/DSHumanStructure.c
@@ -0,0 +1,9 @@
+//
+//  DSHumanStructure.c
+//  DSHomeWork
+//
+//  Created by Denis Surzhan on 18.05.15.
+//  Copyright (c) 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#include "DSHumanStructure.h"
---
+++ DSHomeWork/DSHuman/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  DSHuman
+//
+//  Created by Denis Surzhan on 18.05.15.
+//  Copyright (c) 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "DSHumanStructure.h"
+
+
+int main(int argc, const char * argv[]) {
+   
+    
+    
+    return 0;
+}
