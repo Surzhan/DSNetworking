c22c0f0624db7830e6e79fd14b40c044a2a9a7d1
Denis Surzhan <denis.surzhan@gmail.com>
2015-10-28 23:23:57 +0200

    DLSObservableObject.m -> said the final version of the class

--- DSHomeWork/DLSUIKit/DLSiOSProject/Source/Library/DLSClasses/DLSObservableObject/DLSObservableObject.h
+++ DSHomeWork/DLSUIKit/DLSiOSProject/Source/Library/DLSClasses/DLSObservableObject/DLSObservableObject.h
@@ -9,12 +9,22 @@
 #import <Foundation/Foundation.h>
 
 @interface DLSObservableObject : NSObject
-@property (nonatomic, readonly) NSSet   *observersSet;
+@property (nonatomic, assign)   NSUInteger  state;
+@property (nonatomic, readonly) NSSet       *observersSet;
+
+- (void)setState:(NSUInteger)state withObject:(id)object;
 
 - (void)addObserver:(id)observer;
 - (void)removeObserver:(id)observer;
 - (BOOL)isObservedByObject:(id)observer;
 
+- (void)notifyObservers;
+- (void)notifyObserversWithObject:(id)object;
 - (void)notifyObserversWithSelector:(SEL)selector withObject:(id)object;
+
+- (void)performBlockWithNotification:(void(^)(void))block;
+- (void)performBlockWithoutNotification:(void(^)(void))block;
+
+- (SEL)selectorForState:(NSUInteger)state;
 
 @end
--- DSHomeWork/DLSUIKit/DLSiOSProject/Source/Library/DLSClasses/DLSObservableObject/DLSObservableObject.m
+++ DSHomeWork/DLSUIKit/DLSiOSProject/Source/Library/DLSClasses/DLSObservableObject/DLSObservableObject.m
@@ -9,24 +9,32 @@
 #import "DLSObservableObject.h"
 
 @interface DLSObservableObject ()
-@property (nonatomic, retain) NSHashTable *observers;
+@property (nonatomic, retain)   NSHashTable *mutableObservers;
+@property (nonatomic, assign)   BOOL        shouldNotify;
+
+- (void)performBlock:(void(^)(void))block shouldNotify:(BOOL)shouldNotify;
 
 @end
 
 @implementation DLSObservableObject
 
+@synthesize state = _state;
+
+@dynamic observersSet;
+
 #pragma mark -
 #pragma mark Initializations and Deallocations
 
 - (void)dealloc {
-    self.observers = nil;
+    self.mutableObservers = nil;
 }
 
 - (instancetype)init {
     self = [super init];
     
     if (self) {
-        self.observers = [NSHashTable weakObjectsHashTable];
+        self.mutableObservers = [NSHashTable weakObjectsHashTable];
+        self.shouldNotify = YES;
     }
     
     return self;
@@ -36,26 +44,101 @@
 #pragma mark Accessors
 
 - (NSSet *)observersSet {
-    return self.observers.setRepresentation;
+    @synchronized(self) {
+        return self.mutableObservers.setRepresentation;
+    }
+}
+
+- (void)setState:(NSUInteger)state {
+    [self setState:state withObject:nil];
+}
+
+- (NSUInteger)state {
+    @synchronized(self) {
+        return _state;
+    }
 }
 
 #pragma mark -
 #pragma mark Public Methods
 
+- (void)setState:(NSUInteger)state withObject:(id)object {
+    @synchronized(self) {
+        if (_state != state) {
+            _state = state;
+        }
+        
+        if (self.shouldNotify) {
+            [self notifyObserversWithObject:object];
+        }
+    }
+}
+
 - (void)addObserver:(id)observer {
-    [self.observers addObject:observer];
+    @synchronized(self) {
+        [self.mutableObservers addObject:observer];
+    }
 }
 
 - (void)removeObserver:(id)observer {
-    [self.observers removeObject:observer];
+    @synchronized(self) {
+        [self.mutableObservers removeObject:observer];
+    }
 }
 
 - (BOOL)isObservedByObject:(id)observer {
-    return [self.observers containsObject:observer];
+    @synchronized(self) {
+        return [self.mutableObservers containsObject:observer];
+    }
 }
 
+- (void)notifyObservers;{
+    [self notifyObserversWithObject:nil];
+}
+
+- (void)notifyObserversWithObject:(id)object {
+    [self notifyObserversWithSelector:[self selectorForState:self.state] withObject:object];
+}
+
+#pragma clang dagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+
 - (void)notifyObserversWithSelector:(SEL)selector withObject:(id)object {
- 
+    for (id observer in self.mutableObservers) {
+        if ([observer respondsToSelector:selector]) {
+            [observer performSelector:selector withObject:self withObject:object];
+        }
+    }
+}
+
+#pragma clang diagnostic pop
+
+- (void)performBlockWithNotification:(void(^)(void))block {
+    [self performBlock:block shouldNotify:YES];
+}
+
+- (void)performBlockWithoutNotification:(void(^)(void))block {
+    [self performBlock:block shouldNotify:NO];
+}
+
+- (SEL)selectorForState:(NSUInteger)state {
+    return nil;
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)performBlock:(void(^)(void))block shouldNotify:(BOOL)shouldNotify {
+    @synchronized(self) {
+        BOOL state = self.shouldNotify;
+        self.shouldNotify = shouldNotify;
+        
+        if (block) {
+            block();
+        }
+        
+        self.shouldNotify = state;
+    }
 }
 
 @end
