f16a929c070b63f43ddb3198484ab2e38a0c0e37
Denis Surzhan <denis.surzhan@gmail.com>
2015-10-29 01:43:57 +0200

    DLSUserCell.xib -> added

---
+++ DSHomeWork/DLSUIKit/DLSiOSProject/Source/UI/Views/DLSUserCell/DLSUserCell.h
@@ -0,0 +1,13 @@
+//
+//  DLSUserCell.h
+//  DLSiOSProject
+//
+//  Created by Denis Surzhan on 29.10.15.
+//  Copyright © 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#import "DLSTableViewCell.h"
+
+@interface DLSUserCell : DLSTableViewCell
+
+@end
---
+++ DSHomeWork/DLSUIKit/DLSiOSProject/Source/UI/Views/DLSUserCell/DLSUserCell.m
@@ -0,0 +1,13 @@
+//
+//  DLSUserCell.m
+//  DLSiOSProject
+//
+//  Created by Denis Surzhan on 29.10.15.
+//  Copyright © 2015 IDAPGroupCollege. All rights reserved.
+//
+
+#import "DLSUserCell.h"
+
+@implementation DLSUserCell
+
+@end
